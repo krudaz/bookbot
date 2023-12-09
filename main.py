25c505a4b7566a7ff96c26ae81f425b51ec1e916
krudaz <mark.harris3@gmail.com>
2023-12-09 00:42:34 +0000

    CLI argument, file read and print

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+#!/usr/bin/python3
+
+import sys
+
+
+def main(filename):
+
+    with open(filename, 'r') as file:
+        print(file.read())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
