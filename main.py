496a2ea29a1c21c581ce84a5533294fe36ab7069
krudaz <mark.harris3@gmail.com>
2023-12-09 22:16:30 +0000

    added report logic and formatting

--- main.py
+++ main.py
@@ -9,35 +9,42 @@ def get_input(filename):
     return lines
 
 
-def print_book(lines):
-    for line in lines:
-        print(line)
-
-
 def count_words(lines):
     count = 0
     for line in lines:
         words = line.split()
         for word in words:
             count += 1
-    print(f"Total Words = {count}")
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count} words found in the document ")
+    print("")
 
 
 def count_letters(lines):
+    ordered_keys = []
     alphabet = {"a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0,
                 "i": 0, "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0,
                 "q": 0, "r": 0, "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0,
                 "y": 0, "z": 0}
+
     for line in lines:
         for letter in line.lower():
             if letter in alphabet:
                 alphabet[letter] = alphabet.get(letter) + 1
-    print(alphabet)
+
+    ordered_values = list(alphabet.values())
+    ordered_values.sort()
+
+    for order in ordered_values:
+        ordered_keys.append(list(alphabet.keys())[list(alphabet.values()).index(order)])
+
+    for i in range(25, -1, -1):
+        print(f"The {ordered_keys[i]} character was found {ordered_values[i]} times")
+    print("--- End report ---")
 
 
 def main(filename):
     lines = get_input(filename)
-    print_book(lines)
     count_words(lines)
     count_letters(lines)
 
